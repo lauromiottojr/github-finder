ae94b071926fef8c4b150782584d03be9460bd3e
lauromiottojr <junior.miotto36@gmail.com>
2022-12-28 15:46:49 -0300

    Config request github API

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,21 @@
+import { useState } from 'react'
+
 import Search from '../components/Search'
 
+import { UserProps } from '../types/User';
+
 const Home = () => {
+
+    const [user, setUser] = useState<UserProps | null>(null); // a sintaxe '<UserProps | null>' significa que o o tipo será userprops ou null
+    const loadUser = async (userName: string) => {
+        const res = await fetch(`https://api.github.com/users/${userName}`)
+        const data = await res.json()
+        console.log(data)
+    }
+
     return (
         <div>
-            <Search />
+            <Search loadUser={loadUser} />
         </div>
     )
 }
