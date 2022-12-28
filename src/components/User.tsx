022fa6c2f1ec656c2ba8ac076cc6c232cde5a229
lauromiottojr <junior.miotto36@gmail.com>
2022-12-28 16:51:45 -0300

    User styles

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -4,22 +4,24 @@ import { MdLocationPin } from 'react-icons/md'
 
 import { Link } from "react-router-dom"
 
+import styles from './User.module.css'
+
 const User = ({ login, avatar_url, location, followers, following }: UserProps) => {
     return (
-        <div>
+        <div className={styles.user}>
             <img src={avatar_url} alt={login} />
             <h2>{login}</h2>
             {location && (
-                <p><MdLocationPin /><span>{location}</span></p>
+                <p className={styles.location}><MdLocationPin /><span>{location}</span></p>
             )}
-            <div>
+            <div className={styles.stats}>
                 <div>
                     <p>Seguidores:</p>
-                    <p>{followers}</p>
+                    <p className={styles.number}>{followers}</p>
                 </div>
                 <div>
                     <p>Seguindo:</p>
-                    <p>{following}</p>
+                    <p className={styles.number}>{following}</p>
                 </div>
             </div>
             <Link to={`/repos/${login}`}>Ver melhores projetos</Link>
