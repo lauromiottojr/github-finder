450a6d2cbd96492ceee17bc08b92449810cef695
lauromiottojr <junior.miotto36@gmail.com>
2022-12-28 16:22:09 -0300

    Config search with ENTER

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,7 +2,7 @@ type SearchProps = {
     loadUser: (userName: string) => Promise<void>
 }
 
-import { useState } from 'react'
+import { useState, KeyboardEvent } from 'react'
 
 import { BsSearch } from 'react-icons/bs'
 
@@ -12,12 +12,18 @@ const Search = ({ loadUser }: SearchProps) => {
 
     const [userName, setUserName] = useState("")
 
+    const handleKeyDown = (e: KeyboardEvent) => {
+        if (e.key === "Enter") {
+            loadUser(userName)
+        }
+    }
+
     return (
         <div className={styles.search}>
             <h2>Busque por um usuário:</h2>
             <p>Conheça seus melhores repositórios</p>
             <div className={styles.searchContainer}>
-                <input type="text" placeholder='Digite o nome do usuário' onChange={(e) => setUserName(e.target.value)} />
+                <input type="text" placeholder='Digite o nome do usuário' onChange={(e) => setUserName(e.target.value)} onKeyDown={handleKeyDown} />
                 <button onClick={() => loadUser(userName)}><BsSearch /></button>
             </div>
         </div>
