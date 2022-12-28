c6c0f112ff536add77fccbe2bfbfac6bd88be140
lauromiottojr <junior.miotto36@gmail.com>
2022-12-28 16:04:46 -0300

    Config styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom"
 
+import styles from './App.module.css'
+
 function App() {
 
   return (
-    <div className="App">
+    <div className={styles.app}>
       <h1>github finder</h1>
       <Outlet />
     </div>
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -6,15 +6,17 @@ import { useState } from 'react'
 
 import { BsSearch } from 'react-icons/bs'
 
+import styles from './Search.module.css'
+
 const Search = ({ loadUser }: SearchProps) => {
 
     const [userName, setUserName] = useState("")
 
     return (
-        <div>
+        <div className={styles.search}>
             <h2>Busque por um usuário:</h2>
             <p>Conheça seus melhores repositórios</p>
-            <div>
+            <div className={styles.searchContainer}>
                 <input type="text" placeholder='Digite o nome do usuário' onChange={(e) => setUserName(e.target.value)} />
                 <button onClick={() => loadUser(userName)}><BsSearch /></button>
             </div>
