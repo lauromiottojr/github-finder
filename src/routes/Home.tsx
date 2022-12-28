4eb6c552818ffbdb8a259dddc64bc3507816c948
lauromiottojr <junior.miotto36@gmail.com>
2022-12-28 16:41:18 -0300

    404 error config

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,15 +2,25 @@ import { useState } from 'react'
 
 import Search from '../components/Search'
 import User from '../components/User'
+import Error from '../components/Error';
 
 import { UserProps } from '../types/User';
 
 const Home = () => {
 
     const [user, setUser] = useState<UserProps | null>(null); // a sintaxe '<UserProps | null>' significa que o o tipo será userprops ou null
+    const [error, setError] = useState(false)
     const loadUser = async (userName: string) => {
+        setError(false)
+        setUser(null)
         const res = await fetch(`https://api.github.com/users/${userName}`)
         const data = await res.json()
+
+        if (res.status === 404) {
+            setError(true)
+            return;
+        }
+
         const { avatar_url, login, location, followers, following } = data
         const userData: UserProps = {
             avatar_url,
@@ -26,6 +36,7 @@ const Home = () => {
         <div>
             <Search loadUser={loadUser} />
             {user && <User {...user} />}
+            {error && <Error />}
         </div>
     )
 }
